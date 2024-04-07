d8d4efb414823b1be1dbb2ffee1d08c97571cd5c
NestorRian <nestor.rian2004@gmail.com>
2024-04-07 18:08:36 -0300

    Gerenciamento

---
+++ gerenciamentoFincanceiro.js
@@ -0,0 +1,40 @@
+class Lancamento {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+// Exemplo de uso
+
+const salario = new Lancamento('Salario', 5000)
+const contaDeLuz = new Lancamento('Luz', -220)
+const contaDeAgua = new Lancamento('Água', -150)
+const contaDeInternet = new Lancamento('Internet', -150)
+const faturaDoCartao = new Lancamento('Fatura', -2000)
+
+
+const contas = new CicloFinanceiro(7, 2024)
+contas.addLancamentos(salario, contaDeLuz, contaDeAgua, contaDeInternet, faturaDoCartao)
+
+console.log('Valor restante:', contas.sumario())
